244427670c931f78cbe3753adedddfad80582e2f
Gustavo Macedo <mota.macedo05@gmail.com>
2024-05-20 18:54:10 -0300

    Possible moves of a piece

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,10 +1,10 @@
 package boardgame;
 
-public class Piece {
-	
+public abstract class Piece {
+
 	protected Position position;
 	private Board board;
-	
+
 	public Piece(Board board) {
 		this.board = board;
 		this.position = null;
@@ -13,7 +13,26 @@ public class Piece {
 	protected Board getBoard() {
 		return board;
 	}
+
+	public abstract boolean[][] possibleMoves();
+
 	
-	
-	
+	// Exemplo de implementação padrão concreta que depende de um método abstrato.
+	public boolean possibleMove(Position position) {
+		return possibleMoves()[position.getRow()][position.getColumn()];
+	}
+
+
+	// Exemplo de implementação padrão concreta que depende de um método abstrato.
+	public boolean isThereAnyPossibleMove() {
+		boolean[][] aux = possibleMoves();
+		for (int i = 0; i < aux.length; i++) {
+			for (int j = 0; j < aux.length; j++) {
+				if (aux[i][j]) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
 }
