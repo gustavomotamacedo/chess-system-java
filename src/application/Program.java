d512ef750a7eac7d73d7e8ec1342ea92b0c36cfd
Gustavo Macedo <mota.macedo05@gmail.com>
2024-06-04 20:31:52 -0300

    Update special move: Promotion

--- src/application/Program.java
+++ src/application/Program.java
@@ -40,7 +40,12 @@ public class Program {
 				
 				if(chessMatch.getPromoted() != null) {
 					System.out.print("Enter piece for promotion (B/N/R/Q): ");
-					String type = sc.nextLine();
+					String type = sc.nextLine().toUpperCase();
+					while (!type.equals("B") && !type.equals("N") && !type.equals("Q") && !type.equals("R")) {
+						System.out.println("Invalid value.");
+						System.out.print("Enter piece for promotion (B/N/R/Q): ");
+						type = sc.nextLine().toUpperCase();
+					}
 					chessMatch.replacePromotedPiece(type);
 				}
 			} catch (ChessException e) {
